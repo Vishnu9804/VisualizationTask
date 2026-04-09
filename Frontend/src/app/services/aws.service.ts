42938850d21998b1edc1c04071af2dce9d0abf78
Vishnu9804 <burkhawalavishnu@gmail.com>
2026-04-10 04:17:20 +0530

    added

--- Frontend/src/app/services/aws.service.ts
+++ Frontend/src/app/services/aws.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AwsService {
-  private apiUrl = 'http://localhost:5000/api/aws';
+  private apiUrl = 'http://localhost:3000/api/aws';
 
   constructor(private http: HttpClient) {}
 
